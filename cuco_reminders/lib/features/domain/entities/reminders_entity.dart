7f7e136d195ab739b3dbb82400e62348ddbb49e7
RicMerces <ricardoliveira0703@gmail.com>
2022-11-24 12:17:52 -0300

    cuco-reminders-archi

---
+++ cuco_reminders/lib/features/domain/entities/reminders_entity.dart
@@ -0,0 +1,13 @@
+class RemindersEntity {
+  String title;
+  String description;
+  DateTime data;
+  int prioridade;
+
+  RemindersEntity({
+    required this.title,
+    required this.description,
+    required this.data,
+    required this.prioridade,
+  });
+}
